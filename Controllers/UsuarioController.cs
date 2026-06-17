7b11574ee314739bbc88ea28e54d75dd31b7a8de
cauevinnicius <135167189+cauevinnicius@users.noreply.github.com>
2026-06-16 23:36:41 -0300

    Add files via upload

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using GerenciadorAlunosV2.ViewModels;
+using System.Threading.Tasks;
+
+namespace GerenciadorAlunosV2.Controllers;
+
+public class UsuarioController : Controller
+{
+    // estou realizando o uso de dois repository previamente criados e realizando a leitura da IdentityUser, também já criado previamente pelo identity
+    // user manager: focado em CRUD e demais funcionalidades com dados
+    private readonly UserManager<IdentityUser> _userManager;
+    // o signinmanager: já é um repositório mais voltado a sessão (cookies)
+    private readonly SignInManager <IdentityUser> _signInManager;
+    
+    // faço a injeção de dependencia dos repositórios
+    public UsuarioController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Registrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Registrar(RegistroViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // faço a criação de uma variável e instancio a Identity User. Vou usar username e email como email
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+
+            // o repositório do usermanager já tem um método de criar!
+            var result = await _userManager.CreateAsync(user, model.Senha);
+
+            // dps de registrar, se deu tudo certo, já deixo o usuário logado - realizar uma validação de email acho q seria bala...
+            if (result.Succeeded)
+            {
+                // fui buscar mais explicações do pq do isPersistent: se deixamos true, o signin fica gravado pelos cookies 
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                
+                // redireciona o usuário p index da home
+                return RedirectToAction("Index", "Home"); 
+            }
+
+            
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+        
+        return View(model);
+    }
+}
+
+    
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using GerenciadorAlunosV2.Contexts;
 using GerenciadorAlunosV2.Repositories;
 
@@ -8,12 +9,35 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<GerenciadorAlunosDbContext>(options => options.UseMySQL(connectionString));
 
+// agora com o Identity, tenho que builder os seus servicos. Faço de usuário padrão e perfis.
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => 
+{
+    // aproveitei pra já passar uns parâmetros
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+})
+    .AddEntityFrameworkStores<GerenciadorAlunosDbContext>()
+    .AddDefaultTokenProviders(); 
+
+// daí agora preciso do cookie de autenticação
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // se um usuário não logado tentar acessar uma página bloqueada, volta pro login
+    options.LoginPath = "/Conta/Login";
+    // se um usuário tentar acessar uma página q não tem privilégios suficientes, é direcionado p acesso negado
+    options.AccessDeniedPath = "/Conta/AcessoNegado";
+    
+});
 
 // preciso registrar meus repositorios na injeção de dependência do aspnet
 // o addscoped cria uma instância do repositório por requisição web
 builder.Services.AddScoped<AlunoRepository>();
 builder.Services.AddScoped<MensalidadeRepository>();
-builder.Services.AddScoped<UsuarioRepository>();
 
 // preciso incluir os servicos do padrão mvc, q seriam minhas controllers e minhas views
 builder.Services.AddControllersWithViews();
@@ -30,7 +54,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -38,7 +62,7 @@ app.MapStaticAssets();
 // definição da minha rota padrão
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Usuario}/{action=Registrar}/{id?}")
     .WithStaticAssets();
 
 
